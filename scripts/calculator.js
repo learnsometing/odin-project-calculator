83fc76eb6d71a9ac4607bb53cd88cb85319b9ad6
learnsometing <brianmonaccio@gmail.com>
2018-12-08 16:02:19 -0500

    Wrote basic math functions, laid out the grid-template for the calculator, included basic styling

---
+++ scripts/calculator.js
@@ -0,0 +1,45 @@
+function add(number1, number2){
+    let result = number1 + number2;
+    return result;
+}
+
+console.log(add(100, 2));
+
+function subtract(number1, number2){
+    let result = number1 - number2;
+    return result;
+}
+
+console.log(subtract(1, 100));
+
+function multiply(number1, number2){
+    let result = number1 * number2;
+    return result;
+}
+
+console.log(multiply(1, -2));
+
+function divide(number1, number2){
+    let result = number1 / number2;
+    return result;
+}
+
+console.log(divide(2, 0))
+
+function operate(number1, number2, operator){
+    let result = 0;
+    if (operator == '+'){
+        result = add(number1, number2)
+    }
+    else if (operator == '-'){
+        result = subtract(number1, number2)
+    }
+    else if (operator == '*'){
+        result = multiply(number1, number2)
+    }
+    else {
+        result = divide(number1, number2)
+    }
+    return result;
+}
+
