0791798c0ca70f484a22aa88cfca4c9dc2837ea1
learnsometing <brianmonaccio@gmail.com>
2018-12-11 06:51:38 -0500

    Designed the layout of the buttons and added logic to the createButtons function to designate each button's ID and innerText attributes.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -44,12 +44,78 @@ function operate(number1, number2, operator){
 }
 
 function createButtons(){
-    let buttonsContainer = document.getElementById("buttons"),
+    let numbersContainer = document.getElementById("numbers"),
+        operatorsContainer = document.getElementById('operators'),
         button;
     for (let numButtons = 0; numButtons < 20; numButtons++){
         button = document.createElement('button');
-        button.innerText = numButtons;
-        buttonsContainer.appendChild(button);
+        
+        if (numButtons < 15){
+            if (numButtons < 10){
+                button.id = numButtons;
+                button.innerText = numButtons;
+            }
+            else if (numButtons == 10){
+                button.id = 'decimal';
+                button.innerText = '.';
+            }
+            else if (numButtons == 11){
+                button.id = 'negative-sign';
+                button.innerText = '(-)';
+            }
+            else if (numButtons == 12){
+                button.id = 'clear'
+                button.innerText = 'Clear'
+            }
+            else if (numButtons == 13){
+                button.id = 'exponent';
+                button.innerText = '^';
+            }
+            else {
+                button.id = 'delete'
+                button.innerText = 'Delete'
+            }
+            numbersContainer.appendChild(button);
+        }
+        else {
+            if (numButtons == 15){
+                button.id = 'divide';
+                button.innerText = '/';
+            }
+            else if (numButtons == 16){
+                button.id = 'multiply';
+                button.innerText = '*'
+            }
+            else if (numButtons == 17){
+                button.id = 'subtract';
+                button.innerText = '-'
+            }
+            else if (numButtons == 18){
+                button.id = 'add';
+                button.innerText = '+';
+            }
+            else {
+                button.id = 'enter';
+                button.innerText = 'Enter'
+            }
+            operatorsContainer.appendChild(button);
+        }
+    }
+    
+}
+
+function labelButtons(){
+    if (numButtons == 3){
+        button.innerText = 'Clear';
+    }
+    else if (numButtons == 7) {
+        button.innerText = '/';
+    }
+    else if (numButtons == 11){
+        button.innerText = '*';
+    }
+    else if (numButtons){
+
     }
 }
     
