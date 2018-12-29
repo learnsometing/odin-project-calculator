33aafbf56105f2034e6ff579771e6f43f0f14379
learnsometing <brianmonaccio@gmail.com>
2018-12-29 13:20:34 -0500

    Commit before changing the way the math and displayare handled

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,39 +1,40 @@
-let result;
-    
+let result,
+text = document.querySelector('p'),
+textString = text.innerText,
+textArray,
+textRect = text.getBoundingClientRect(),
+textWidth = textRect.width,
+display = document.getElementById('display'),
+displayRect = display.getBoundingClientRect(),
+displayWidth = displayRect.width;
+
+const operatorRegExp = /[*/^+-]/;
+
+
 function add(number1, number2){
     result = number1 + number2;
     return result;
 }
-
-// console.log(add(100, 2));
 
 function subtract(number1, number2){
     result = number1 - number2;
     return result;
 }
 
-// console.log(subtract(1, 100));
-
 function multiply(number1, number2){
     result = number1 * number2;
     return result;
 }
-
-// console.log(multiply(1, -2));
 
 function divide(number1, number2){
     result = number1 / number2;
     return result;
 }
 
-// console.log(divide(2, 0));
-
 function exponent(number1, number2){
     result = number1 ** number2;
     return result;
 }
-
-// console.log(exponent(2, 2));
 
 function operate(number1, operator, number2){
     if (operator == '+'){
@@ -51,16 +52,22 @@ function operate(number1, operator, number2){
     else {
         result = divide(number1, number2)
     }
+
+    Math.round(result).toPrecision(4);
     return result;
 }
+
+/**********************************************************************************************************************************/
 
 function createButtons(){
     let numbersContainer = document.getElementById("numbers"),
         operatorsContainer = document.getElementById('operators'),
         button;
+
     for (let numButtons = 0; numButtons < 20; numButtons++){
         button = document.createElement('button');
-        button.id = numButtons
+        button.id = numButtons;
+
         if (button.id < 15){
             numbersContainer.appendChild(button);
         }
@@ -70,78 +77,78 @@ function createButtons(){
     }
 }
 
-function labelButtons(){
+function labelAndActivateButtons(){
     let buttons = document.querySelectorAll('button');
 
-    buttons.forEach((button) => {
-        if (button.id < 10){
-            button.innerText = button.id;
-        }
-        else if (button.id == 10){
-            button.id = 'decimal';
-            button.innerText = '.';
-        }
-        else if (button.id == 11){
-            button.id = 'negative-sign';
-            button.innerText = '(- )';
-        }
-        else if (button.id == 12){
-            button.id = 'clear'
-            button.innerText = 'Clear'
-        }
-        else if (button.id == 13){
-            button.id = 'exponent';
-            button.innerText = '^';
-        }
-        else if (button.id == 14){
-            button.id = 'delete'
-            button.innerText = 'Delete'
-        }
-        else if (button.id == 15){
-            button.id = 'divide';
-            button.innerText = '/';
-        }
-        else if (button.id == 16){
-            button.id = 'multiply';
-            button.innerText = '*'
-        }
-        else if (button.id == 17){
-            button.id = 'subtract';
-            button.innerText = '-'
-        }
-        else if (button.id == 18){
-            button.id = 'add';
-            button.innerText = '+';
-        }
-        else {
-            button.id = 'enter';
-            button.innerText = 'Enter'
-        }
-    });
-}
+    function labelButtons(){
+        buttons.forEach((button) => {
+            if (button.id < 10){
+                button.innerText = button.id;
+            }
+            else if (button.id == 10){
+                button.id = 'decimal';
+                button.innerText = '.';
+            }
+            else if (button.id == 11){
+                button.id = 'exponent'
+                button.innerText = '^'
+            }
+            else if (button.id == 12){
+                button.id = 'clear';
+                button.innerText = 'Clear';
+            }
+            else if (button.id == 13){
+                button.id = 'delete'
+                button.innerText = 'Delete'
+            }
+            else if (button.id == 14){
+                button.id = 'sign change';
+                button.innerText = '+/-';
+            }
+            else if (button.id == 15){
+                button.id = 'multiply';
+                button.innerText = '*'
+            }
+            else if (button.id == 16){
+                button.id = 'subtract';
+                button.innerText = '-'
+            }
+            else if (button.id == 17){
+                button.id = 'add';
+                button.innerText = '+';
+            }
+            else if (button.id == 18){
+                button.id = 'divide';
+                button.innerText = '/';
+            }
+            else {
+                button.id = 'enter';
+                button.innerText = 'Enter';
+            }
+        });
+    }
 
-function activateButtons(){
-    let buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', (e) => {clicked(button);});
-    });
+    function activateButtons(){
+        buttons.forEach((button) => {
+            button.addEventListener('click', (e) => {clicked(button);});
+        });
+    }
+
+    labelButtons();
+    activateButtons();
 }
 
 function clicked(button){
-    let buttonText = button.innerText,
-    text = document.querySelector('p'),
-    textString = text.innerText,
-    textRect = text.getBoundingClientRect(),
-    textWidth = textRect.width,
-    display = document.getElementById('display'),
-    displayRect = display.getBoundingClientRect(),
-    displayWidth = displayRect.width;
-
+    let buttonText = button.innerText;
+    
     if (buttonText == 'Clear'){
         clearDisplay();
     }
     else if (buttonText == 'Delete'){
         deleteText();
+    }
+    else if (buttonText == '+/-'){
+        changeSigns();
     }
     else if (buttonText == 'Enter'){
         enter();
@@ -153,67 +160,82 @@ function clicked(button){
         else {
             textString += buttonText;
             text.innerText = textString;
+            storeFormulaInArray();
         }  
     }
 }
 
-function clearDisplay(){
-    let text = document.querySelector('p');
-    
+function clearDisplay(){    
     text.innerText = '';
+    textString = '';
 }
 
 function deleteText(){
-    let text = document.querySelector('p');
-        textString = text.innerText,
-        lastIndex = textString.length - 1;
+    let lastIndex = textString.length - 1;
 
-        textString = textString.slice(0, lastIndex);
-        text.innerText = textString;
+    textString = textString.slice(0, lastIndex);
+    text.innerText = textString;
+}
+
+function changeSigns(){
+    let lastArrayIndex = textArray.length - 1,
+    lastTypedChar = textArray[lastArrayIndex],
+    signOfLastTypedNumber;
+
+    if (!(operatorRegExp.test(lastTypedChar)) || lastTypedChar != '.'){
+        signOfLastTypedNumber = Math.sign(lastTypedChar);
+        if (signOfLastTypedNumber == 1 || signOfLastTypedNumber == -1){
+            lastTypedChar = operate(lastTypedChar, '*', -1);
+        }
+    }
+
+    textArray.splice(lastIndex, 1, lastTypedChar);
+    console.log(textArray);
+
+}
+
+function storeFormulaInArray(){
+    let lastIndex = textString.length - 1,
+    tempArray = [],
+    tempString = '';
+
+    for (let i = 0; i < textString.length; i++){
+        let textCharacter = textString.charAt(i),
+            charIsOperator = operatorRegExp.test(textCharacter);
+
+        if (charIsOperator || i == lastIndex){
+            if (i == lastIndex && !charIsOperator){
+                tempString += textCharacter;
+            }
+
+            tempArray.push(tempString);
+            tempString = '';
+
+            if (charIsOperator){
+                tempArray.push(textCharacter);
+            }                    
+        }
+        else{
+            tempString += textCharacter;
+        }
+    }
+    console.log(tempArray);
+    textArray = tempArray;
+    return tempArray;
 }
 
 function enter(){
-    let text = document.querySelector('p'),
-        textString = text.innerText;
+    // let firstCharacter = textString[0],
+    // lastCharacter = textString[lastIndex],
+    // multipleOperators = /[*/^+-](?=[*/^+-])/;
 
-    function convertToArray(){
-        firstCharacter = textString[0],
-        lastIndex = textString.length - 1,
-        lastCharacter = textString[lastIndex],
-        textArray = [],
-        tempString = '',
-        operator = /[*/^+-]/,
-        multipleOperators = /[*/^+-](?=[*/^+-])/;
+    // const firstCharIsOperator = operator.test(firstCharacter),
+    // multipleOperatorsInARow = multipleOperators.test(textString),
+    // lastCharIsOperator = operator.test(lastCharacter);
 
-        const firstCharIsOperator = operator.test(firstCharacter),
-            multipleOperatorsInARow = multipleOperators.test(textString),
-            lastCharIsOperator = operator.test(lastCharacter);
-
-        if (firstCharIsOperator || multipleOperatorsInARow || lastCharIsOperator){
-            alert ('The formula you entered contains errors. Please re-enter your formula.');
-        }
-        else {
-            for (let i = 0; i < textString.length; i++){
-                let textCharacter = textString.charAt(i),
-                    charIsOperator = operator.test(textCharacter);
-
-                if (charIsOperator || i == lastIndex){
-                    if (!(i == lastIndex)){
-                        textArray.push(tempString);
-                        tempString = '';
-                    }
-
-                    textArray.push(textCharacter);
-                }
-                else{
-                    tempString += textCharacter;
-                }
-            }
-        }
-        console.log(textArray);
-
-        return textArray;
-    }
+    // if (firstCharIsOperator || multipleOperatorsInARow || lastCharIsOperator){
+    //     alert ('The formula you entered contains errors. Please re-enter your formula.');
+    // }
 
     function doTheMath(array){
         let finalAnswer,
@@ -224,14 +246,13 @@ function enter(){
             firstOperand,
             secondOperand,
             operator,
-            operators = /[*^/+-]/,
             exp = '^',
             mult = '*'
             div = '/',
             plus = '+',
             minus = '-';
 
-        while(operators.test(array)){
+        while(operatorRegExp.test(array)){
             if (array.includes(exp)){
                 operatorIndex = array.indexOf(exp);
             }
@@ -244,10 +265,14 @@ function enter(){
             else if (array.includes(plus)){
                 operatorIndex = array.indexOf(plus);
             }
-            else{ //minus
-                operatorIndex = array.indexOf(minus);
+            else{ //subtraction
+                if ((array.length == 1) && (Math.sign(array[0]) == -1)){
+                    break;
+                }
+                else {
+                    operatorIndex = array.indexOf(minus);
+                }
             }
-            console.log(operatorIndex);
 
             firstOperandIndex = operatorIndex - 1;
             secondOperandIndex = operatorIndex + 1;
@@ -260,22 +285,16 @@ function enter(){
             array.splice(firstOperandIndex, 3, tempAnswer);
         }
 
-        console.log(array);
-
         finalAnswer = array.toString();
-        console.log(finalAnswer);
-        Math.round(finalAnswer);
 
-        console.log(finalAnswer);
-
+        textString = '';
         text.innerText = finalAnswer;
         return finalAnswer;
 
     }
 
-    doTheMath(convertToArray());
+    doTheMath(textArray);
 }
 
 createButtons();
-labelButtons();
-activateButtons();
+labelAndActivateButtons();
